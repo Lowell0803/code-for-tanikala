27414ad8b4c6da9bc7637e2df38ddc5282044054
Zarina B. Dungo <2020105248@ms.bulsu.edu.ph>
2025-03-07 17:07:01 +0800

    fixed dropdown of admin

--- public/admin/js/dropdown.js
+++
@@ -1,5 +0,0 @@
-function toggleButton() {
-  const checkbox = document.getElementById("agreement");
-  const button = document.getElementById("proceedBtn");
-  button.disabled = !checkbox.checked; // Enable when checked, disable when unchecked
-}
