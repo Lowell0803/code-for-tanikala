8edfd457dd3f634a60caa297e0c05634a3e65b56
Lowell0803 <aquino.ylt@gmail.com>
2025-03-10 23:24:13 +0800

    the last nonce

--- db.js
+++ db.js
@@ -1,3 +1,22 @@
+// const { MongoClient } = require("mongodb");
+// require("dotenv").config();
+
+// const uri = process.env.MONGODB_URI;
+// const client = new MongoClient(uri);
+
+// async function connectToDatabase() {
+//   try {
+//     await client.connect();
+//     console.log("Connected to MongoDB Atlas");
+//     return client.db("tanikala");
+//   } catch (err) {
+//     console.error("Failed to connect to MongoDB", err);
+//     throw err;
+//   }
+// }
+
+// module.exports = connectToDatabase;
+
 const { MongoClient } = require("mongodb");
 require("dotenv").config();
 
@@ -15,4 +34,4 @@ async function connectToDatabase() {
   }
 }
 
-module.exports = connectToDatabase;
+module.exports = { connectToDatabase, client };
