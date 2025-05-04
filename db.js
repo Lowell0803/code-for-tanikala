da85549e559d0d30f564150588ae4fd60db4dea6
Jajangmyeon <ainajanefranco@gmail.com>
2025-05-04 22:51:29 +0800

    Aina Task Hide Login

--- db.js
+++ db.js
@@ -18,7 +18,7 @@
 // module.exports = connectToDatabase;
 
 const { MongoClient } = require("mongodb");
-require("dotenv").config();
+require("dotenv").config(); 
 
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
